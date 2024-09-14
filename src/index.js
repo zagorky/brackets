80216f527d502fd2c1bdcbf8835958545b23eec6
zagorky <zagorkiynahyi@gmail.com>
2024-09-14 14:37:32 +0300

    feat: the task is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketsMap = {};
+  bracketsConfig.forEach(([open, close]) => {
+    bracketsMap[close] = open;
+  });
+  const openBrackets = bracketsConfig.map((pair) => pair[0]);
+  const closeBrackets = bracketsConfig.map((pair) => pair[1]);
+  for (let i = 0; i < str.length; i++) {
+    const x = str[i];
+    if (openBrackets.includes(x)) {
+      if (closeBrackets.includes(x) && stack[stack.length - 1] === x) {
+        stack.pop();
+      } else {
+        stack.push(x);
+      }
+    } else if (closeBrackets.includes(x)) {
+      if (stack.length === 0 || stack.pop() !== bracketsMap[x]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 };
-console.log(check(config1));
-
-const config3 = [
-  ["(", ")"],
-  ["[", "]"],
-  ["{", "}"],
-];
-const config2 = ("())(", [["(", ")"]]); //false
-const config1 = ("((()))()", [["(", ")"]]); //true
